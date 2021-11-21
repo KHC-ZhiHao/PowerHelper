ea5f8c9cfa137e310bea9ba6d10721720dff98be
zhihao <khczhihao@gmail.com>
2021-11-22 01:59:57 +0800

    new tool

--- lib/utils/flow.ts
+++ lib/utils/flow.ts
@@ -23,3 +23,37 @@ export const randomPick = <T extends any>(items: T[]): T => {
 export const randomInt = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+/** 指定的值如果是 null，則回傳預設值 */
+
+export const ifEmpty = <T>(data: T | undefined, def: T): T => {
+    return (data != null ? data : def)
+}
+
+/** 比 typeof 回傳更精準的類型 */
+
+export const getType = (target: any) => {
+    let type = typeof target
+    if (target == null) {
+        return 'empty'
+    }
+    if (Array.isArray(target)) {
+        return 'array'
+    }
+    if (type === 'number' && isNaN(target)) {
+        return 'NaN'
+    }
+    if (target instanceof RegExp) {
+        return 'regexp'
+    }
+    if (target instanceof Promise) {
+        return 'promise'
+    }
+    if (typeof Buffer !== 'undefined' && Buffer.isBuffer(target)) {
+        return 'buffer'
+    }
+    if (target instanceof Error) {
+        return 'error'
+    }
+    return type
+}
